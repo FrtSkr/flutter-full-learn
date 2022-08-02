f016fa532d00420570f8ced82b251fd2a18fae81
Fırat Şakar <frtskr0@gmail.com>
2022-08-02 06:51:16 +0300

    list view

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_full_learn/101/color_learn.dart';
+import 'package:flutter_full_learn/101/list_view_learn.dart';
 import 'package:flutter_full_learn/101/page_view_learn.dart';
 import 'package:flutter_full_learn/101/stack_learn.dart';
 import 'package:flutter_full_learn/101/statefull_learn.dart';
@@ -49,7 +50,7 @@ class MyApp extends StatelessWidget {
             backgroundColor: Colors.transparent,
             elevation: 0,
           )),
-      home: const ColorLifeCycleDemos(),
+      home: const ListViewLearn(),
     );
   }
 }
