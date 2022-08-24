31a5c8d6b96c520eecc717070e478df682cc08d4
Fırat Şakar <frtskr0@gmail.com>
2022-08-24 21:48:33 +0300

    some changed

--- lib/202/package/loading_bar.dart
+++ lib/202/package/loading_bar.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_full_learn/202/theme/light_theme.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 import '../../demos/MyProjects/login_screen.dart';
 import '../package_learn_view.dart';
+import '../password_text_field.dart';
 
 class LoadingBar extends StatefulWidget {
   const LoadingBar({Key? key}) : super(key: key);
@@ -21,21 +23,27 @@ class _LoadingBarState extends State<LoadingBar> with TickerProviderStateMixin {
     }
     return Scaffold(
       floatingActionButton: FloatingActionButton(
+        backgroundColor: Theme.of(context).buttonTheme.colorScheme?.onPrimary,
         onPressed: () {
-          setState(() {
-            isLoading = true;
-          });
+          // setState(() {
+          //   isLoading = true;
+          // });
         },
         child: const Icon(
           Icons.add,
           color: Colors.white,
         ),
       ),
-      body: const SpinKitFadingCube(
-        color: Colors.red,
-        size: 50.0,
-        //  controller: AnimationController(
-        //     vsync: this, duration: const Duration(milliseconds: 1200)),
+      body: Column(
+        children: const [
+          SpinKitChasingDots(
+            color: Colors.red,
+            size: 50.0,
+            //  controller: AnimationController(
+            //     vsync: this, duration: const Duration(milliseconds: 1200)),
+          ),
+          PasswordTextField(),
+        ],
       ),
     );
   }
--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ import 'package:flutter_full_learn/101/statefull_learn.dart';
 import 'package:flutter_full_learn/101/statefull_life_cycle_learn.dart';
 import 'package:flutter_full_learn/101/text_field_learn.dart';
 import 'package:flutter_full_learn/202/tab_learn.dart';
+import 'package:flutter_full_learn/202/theme/light_theme.dart';
 import 'package:flutter_full_learn/demos/MyProjects/login_screen.dart';
 import 'package:flutter_full_learn/demos/MyProjects/my_account.dart';
 import 'package:flutter_full_learn/demos/color_demos_view.dart';
@@ -19,6 +20,7 @@ import '101/navigation_learn.dart';
 import '202/model_learn_view.dart';
 import '202/package/loading_bar.dart';
 import '202/package_learn_view.dart';
+import '202/password_text_field.dart';
 import '202/service/service_learn_view.dart';
 import '202/service/service_post_learn_view.dart';
 
@@ -38,37 +40,38 @@ class MyApp extends StatelessWidget {
     //projenin yapısını bozar.
     return MaterialApp(
       title: 'Flutter Demo',
-      theme: ThemeData.dark().copyWith(
-          //TabBar'ı özelleştirdik
-          tabBarTheme: const TabBarTheme(
-            labelColor: Colors.green,
-            unselectedLabelColor: Colors.grey,
-            indicatorSize: TabBarIndicatorSize.label,
-          ),
-          //Uygulamadi tüm card widgetlarının temasını özelleştirdik
-          cardTheme: CardTheme(
-            shape:
-                RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
-          ),
-          //Progredd Indicator için de temayı standart haline getirebiliriz.
-          progressIndicatorTheme: const ProgressIndicatorThemeData(
-            color: Colors.white,
-          ),
-          //Aynı şekilde listTile'ın da temasını standardize ettik.
-          listTileTheme:
-              const ListTileThemeData(contentPadding: EdgeInsets.zero),
-          //Uygulamadaki errorColor'ı standart haline getirdik
-          errorColor: ColorsItems.sulu,
-          //AppBarımızı belirli standartlarda oluşturduk
-          appBarTheme: const AppBarTheme(
-            centerTitle: true,
-            //Sistem viewlarının stillerini ayarlar
-            systemOverlayStyle: SystemUiOverlayStyle.light,
-            //Transparan ve elevation'ı 0 verirsek sayfayla birebir AppBar olur. Güzel de olur :))
-            backgroundColor: Colors.transparent,
-            elevation: 0,
-          )),
-      home: const PackageLearnView(),
+      theme: LightTheme().theme,
+      // theme: ThemeData.dark().copyWith(
+      //     //TabBar'ı özelleştirdik
+      //     tabBarTheme: const TabBarTheme(
+      //       labelColor: Colors.green,
+      //       unselectedLabelColor: Colors.grey,
+      //       indicatorSize: TabBarIndicatorSize.label,
+      //     ),
+      //     //Uygulamadi tüm card widgetlarının temasını özelleştirdik
+      //     cardTheme: CardTheme(
+      //       shape:
+      //           RoundedRectangleBorder(borderRadius: BorderRadius.circular(20)),
+      //     ),
+      //     //Progredd Indicator için de temayı standart haline getirebiliriz.
+      //     progressIndicatorTheme: const ProgressIndicatorThemeData(
+      //       color: Colors.white,
+      //     ),
+      //     //Aynı şekilde listTile'ın da temasını standardize ettik.
+      //     listTileTheme:
+      //         const ListTileThemeData(contentPadding: EdgeInsets.zero),
+      //     //Uygulamadaki errorColor'ı standart haline getirdik
+      //     errorColor: ColorsItems.sulu,
+      //     //AppBarımızı belirli standartlarda oluşturduk
+      //     appBarTheme: const AppBarTheme(
+      //       centerTitle: true,
+      //       //Sistem viewlarının stillerini ayarlar
+      //       systemOverlayStyle: SystemUiOverlayStyle.light,
+      //       //Transparan ve elevation'ı 0 verirsek sayfayla birebir AppBar olur. Güzel de olur :))
+      //       backgroundColor: Colors.transparent,
+      //       elevation: 0,
+      //     )),
+      home: const LoadingBar(),
     );
   }
 }
